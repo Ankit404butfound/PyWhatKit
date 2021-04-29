a3006dde3ec361af59e95546eb8a9d2960bcf56e
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-04-29 14:25:47 +0530

    Create audiobook.py

---
+++ pywhatkit/audiobook.py
@@ -0,0 +1,15 @@
+import PyPDF2
+from gtts import gTTS
+
+def create_audiobook(path,name='pywhatkit_audio_book'):
+    try:
+        book = open(path, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(book)
+        pages = pdfReader.numPages
+    except:
+        raise Exception("couldn't open pdf file")
+    for num in range(pages):
+        page = pdfReader.getPage(num)
+        txt = page.extractText()
+        book_ = gTTS(text=txt,slow=False)
+        book_.save(f'{name}.mp3')
