935bed4195c3eb648e4bf86020687ae26a6d00f7
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-04-29 13:01:25 +0530

    Update __init__.py

--- pywhatkit/__init__.py
+++ pywhatkit/__init__.py
@@ -4,7 +4,7 @@ Pull requests are always welcomed"""
 from pywhatkit.ascii_art import image_to_ascii_art
 from pywhatkit.chr_to_handwriting import text_to_handwriting
 import pywhatkit.help
-from pywhatkit.main import print_sleep_time, check_window, sendwhatmsg, sendwhatmsg_to_group, info, playonyt, search
+from pywhatkit.main import print_sleep_time, check_window, sendwhatmsg, sendwhatmsg_to_group, info, playonyt, search,takescreenshot
 from pywhatkit.sc import shutdown, cancelShutdown
 from pywhatkit.wwd import tutorial_english, tutorial_hindi, developer_contact
 from pywhatkit.history import showHistory
