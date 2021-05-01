97870e1dfc753e53195e7854bfeb4a57d7b3005a
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-05-01 16:44:36 +0530

    Delete pywhatkit directory

--- pywhatkit/history.py
+++
@@ -1,8 +0,0 @@
-def showHistory() -> None:
-    """Prints the information of all the sent messages using this program"""
-    with open("pywhatkit_dbs.txt","r") as f:
-        content = f.read()
-
-    if content == "--------------------":
-        content = None
-    print(content)
