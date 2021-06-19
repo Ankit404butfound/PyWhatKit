f7960c09b70d27e251a1a5b1bb4ce53f9816522e
Himangshu De <76109041+Himangshu-De@users.noreply.github.com>
2021-06-19 11:51:11 +0530

    Updated sc.py with some minor bugs
    Reason for modifying it:
    When I ran the program and called cancel_shutdown function only it worked well and when I ran this program second time and called the same function second time then I got an output that "Unable to abort the system shutdown because no shutdown was in progress.(1116)" after that I got another output from the program "Shutdown has been cancelled!". So the output with numeric code was not from the program but from the windows command line. Now what I actually did is that whenever cancel_shutdown function will be called under first condition it will check with the error code that whether a system shutdown has already scheduled or not and the program under that condition will behave accordingly, the same has been implemented under the shutdown function's first condition[Actually I'm a windows user, so I amended the changes under the "windows" which is set as a condition].

--- pywhatkit/sc.py
+++ pywhatkit/sc.py
@@ -1,41 +1,53 @@
 import platform
 import os
 from typing import NoReturn
-
+import winerror 
 osname = platform.system()
 
 
 def shutdown(time: int = 20) -> NoReturn:
-    """Shutdowns the machine in a given time"""
+    """
+    Gives a shutdown request to the system with the specified time
+
+    Args:
+    `time`: Governs the time for shutdown. Taken as `int` in unit `seconds`. `Specified default value is 20`
+    """
     # For Windows, time should be given in seconds
     # For MacOS and Linux based distributions, time should be given in minutes
-    global osname
-    osname = platform.system()
+
     if "window" in osname.lower():
-        cont = "shutdown -s -t %s" % time
-        os.system(cont)
+        cont = f"shutdown -s -t {time}"
+        ErrorCode = os.system(cont)
+        if ErrorCode == winerror.ERROR_SHUTDOWN_IN_PROGRESS or ErrorCode == 1115:# Here 1115 is the error code of scheduled shutdown.
+            print("A shutdown process has already been scheduled...\nIgnoring this process")
+
+        else:
+            print(f"Your System will shutdown in {time} seconds")
 
     elif "linux" in osname.lower():
-        cont = "shutdown -h %s" % time
+        cont = f"shutdown -h {time}"
         os.system(cont)
 
     elif "darwin" in osname.lower():
-        cont = "shutdown -h -t %s" % time
+        cont = f"shutdown -h -t {time}"
         os.system(cont)
 
     else:
         raise Warning(
-            "This function is for Windows, Mac and Linux users only, can't execute on %s" % osname)
+            "This function is for Windows, Mac and Linux users only, can't execute on {}".format(osname))
 
 
 def cancel_shutdown() -> NoReturn:
-    """Will cancel the scheduled shutdown"""
-    global osname
-    osname = platform.system()
+    """
+    Will cancel the scheduled shutdown
+    """
     if "window" in osname.lower():
         cont = "shutdown /a"
-        os.system(cont)
-        print("Shutdown has been Cancelled!")
+        ErrorCode = os.system(cont)
+        if ErrorCode == winerror.ERROR_NO_SHUTDOWN_IN_PROGRESS:
+            print("ShutDown cancellation process has been aborted! [NO shutdown scheduled]")
+        else:
+            print("ShutDown has been cancelled")
 
     elif "linux" in osname.lower():
         cont = "shutdown -c"
@@ -49,4 +61,4 @@ def cancel_shutdown() -> NoReturn:
 
     else:
         raise Warning(
-            "This function is for Windows and Linux only, can't execute on: %s" % osname)
+            f"This function is for Windows and Linux only, can't execute on: {osname}")
