0f5f81b13214a8a3e6aa0805e66c044b9faeb12b
aaryanrr <raiaryan2422@gmail.com>
2021-07-01 17:15:06 +0530

    Functions to __init__.py

--- pywhatkit/__init__.py
+++ pywhatkit/__init__.py
@@ -5,6 +5,6 @@ from pywhatkit.chr_to_handwriting import text_to_handwriting
 import pywhatkit.help
 from pywhatkit.main import print_sleep_time, check_window, sendwhatmsg, sendwhatmsg_to_group, info, playonyt, search, close_tab, sendwhatmsg_instantly, sendwhats_image
 from pywhatkit.sc import shutdown, cancel_shutdown
-from pywhatkit.wwd import tutorial_english, tutorial_hindi, developer_contact
+from pywhatkit.wwd import tutorial_english, tutorial_hindi, developer_contact, join_discord
 from pywhatkit.history import show_history
 from pywhatkit.mail import send_mail, send_hmail
