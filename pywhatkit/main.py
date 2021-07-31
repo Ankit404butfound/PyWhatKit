8ab264bd42b6f621f71c5ba3419c324e9c07c8e8
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-07-31 20:14:02 +0530

    Update main.py

--- pywhatkit/main.py
+++ pywhatkit/main.py
@@ -207,7 +207,7 @@ def sendwhats_image(phone_no: str, img_path: str, caption: str = " ", wait_time:
 
         image = Image.open(img_path)
         output = BytesIO()
-        image.convert('RGB').save(output, "RGB")
+        image.convert('RGB').save(output, "BMP")
         data = output.getvalue()[14:]
         output.close()
         win32clipboard.OpenClipboard()
