9ced9f30e63adadb413f05c46821f7b9b1efc8d3
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-08-11 08:58:21 +0530

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,1 +1,3 @@
-
+@echo off
+if (pip install . && echo 'successfuly install pywhatkit') else if (easy_install pip && pip install . && echo 'successfuly installed pywhatkit') else (echo 'Unable to install' )
+exit
