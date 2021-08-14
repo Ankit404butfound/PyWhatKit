5167246d16b3ede0658a49fe2ebbcd2e46267d46
aaryanrr <raiaryan2422@gmail.com>
2021-08-14 22:04:34 +0530

    Removed wwd.py as it was Unused

--- pywhatkit/__init__.py
+++ pywhatkit/__init__.py
@@ -1,4 +1,7 @@
-"""Pull Requests are always Welcomed"""
+# Version 5.2
+# Status: Stable
+# Documentation: https://github.com/Ankit404butfound/PyWhatKit/wiki
+# Report Bugs and Feature Requests here: https://github.com/Ankit404butfound/PyWhatKit/issues
 
 import pywhatkit.help
 from pywhatkit.ascii_art import image_to_ascii_art
@@ -7,4 +10,3 @@ from pywhatkit.history import show_history
 from pywhatkit.mail import send_mail, send_hmail
 from pywhatkit.main import print_sleep_time, check_window, sendwhatmsg, sendwhatmsg_to_group, info, playonyt, search, close_tab, sendwhatmsg_instantly, sendwhats_image
 from pywhatkit.sc import shutdown, cancel_shutdown
-from pywhatkit.wwd import tutorial_english, tutorial_hindi, developer_contact, join_discord
