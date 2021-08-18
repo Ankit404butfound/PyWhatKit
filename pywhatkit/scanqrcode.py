76cbdaac7d51074a98648c8bcaffa42f302ff17b
AKpython <72851630+akpythonyt@users.noreply.github.com>
2021-08-18 11:25:13 +0530

    QRcode recognition feature
    Its handy feature of mine contributing to pywhatkit

---
+++ pywhatkit/scanqrcode.py
@@ -0,0 +1,20 @@
+import cv2
+import webbrowser
+
+def scan():
+	cap = cv2.VideoCapture(0)
+	detector = cv2.QRCodeDetector()
+	while True:
+		 _, img = cap.read()
+		 data, bbox, _ = detector.detectAndDecode(img)
+		 if data:
+		 	 a=data
+		 	 break
+		 cv2.imshow("QRCODEscanner", img)  
+		 if cv2.waitKey(1) == ord("q"):
+		 	break
+	b=webbrowser.open(str(a))
+	cap.release()
+	cv2.destroyAllWindows()
+
+scan()
