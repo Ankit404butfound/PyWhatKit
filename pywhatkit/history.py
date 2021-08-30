04304d1a9292cb239baadc799d21a029c8609444
aaryanrr <raiaryan2422@gmail.com>
2021-08-30 13:01:02 +0530

    Removed Old Files

--- pywhatkit/history.py
+++
@@ -1,9 +0,0 @@
-def show_history() -> None:
-    """Prints the information of all the sent messages using this program"""
-
-    with open("pywhatkit_dbs.txt", "r") as f:
-        content = f.read()
-
-    if content == "--------------------":
-        content = None
-    print(content)
