4ce55481c4c768863fbbc6d1d6b5faf0a9f4f005
aaryanrr <raiaryan2422@gmail.com>
2021-09-05 22:16:02 +0530

    ✨ Import New Functions

--- pywhatkit/__init__.py
+++ pywhatkit/__init__.py
@@ -5,8 +5,13 @@
 
 import pywhatkit.help
 from pywhatkit.ascii_art import image_to_ascii_art
-from pywhatkit.chr_to_handwriting import text_to_handwriting
-from pywhatkit.history import show_history
+from pywhatkit.handwriting import text_to_handwriting
 from pywhatkit.mail import send_mail, send_hmail
-from pywhatkit.main import print_sleep_time, check_window, sendwhatmsg, sendwhatmsg_to_group, info, playonyt, search, close_tab, sendwhatmsg_instantly, sendwhats_image
+from pywhatkit.whats import (
+    sendwhatmsg,
+    sendwhatmsg_to_group,
+    sendwhatmsg_instantly,
+    sendwhats_image,
+    open_web,
+)
 from pywhatkit.sc import shutdown, cancel_shutdown
