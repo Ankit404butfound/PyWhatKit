543305e1483de158d64605c471df53eaab61069d
aaryanrr <raiaryan2422@gmail.com>
2021-09-05 20:49:33 +0530

    ✨ Improved Logging

---
+++ pywhatkit/core/log.py
@@ -0,0 +1,43 @@
+import time
+import os
+
+from pywhatkit.core.core import check_number
+
+
+def format_message(message: str) -> str:
+    """Formats the Message to remove redundant Spaces and Newline chars"""
+    msg_l = message.split(" ")
+    new = []
+    for x in msg_l:
+        if "\n" in x:
+            x = x.replace("\n", "")
+            new.append(x) if not len(x) == 0 else None
+
+        elif len(x) != 0:
+            new.append(x)
+
+    return " ".join(new)
+
+
+def log_message(_time: time.struct_time, receiver: str, message: str) -> None:
+    """Logs the Message Information after it is Sent"""
+
+    if not os.path.exists("PyWhatKit_DB.txt"):
+        file = open("PyWhatKit_DB.txt", "w+")
+        file.close()
+
+    message = format_message(message)
+
+    with open("PyWhatKit_DB.txt", "a", encoding="utf-8") as file:
+        if check_number(receiver):
+            file.write(
+                f"Date: {_time.tm_mday}:{_time.tm_mon}:{_time.tm_year}\nTime: {_time.tm_hour}:{_time.tm_min}\nPhone "
+                f"Number: {receiver}\nMessage: {message}"
+            )
+        else:
+            file.write(
+                f"Date: {_time.tm_mday}:{_time.tm_mon}:{_time.tm_year}\nTime: {_time.tm_hour}:{_time.tm_min}\n"
+                f"Group ID: {receiver}\nMessage: {message}"
+            )
+        file.write("\n--------------------\n")
+        file.close()
