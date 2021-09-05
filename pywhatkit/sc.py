0fe0d9d2e6376a8cf1dd788ecbcb005f3de6628a
aaryanrr <raiaryan2422@gmail.com>
2021-09-05 20:51:37 +0530

    ♻️ Remove Extra Variables

--- pywhatkit/sc.py
+++ pywhatkit/sc.py
@@ -1,25 +1,25 @@
 import platform
 import os
+
 try:
     import winerror
 except ImportError or ModuleNotFoundError:
     pass
 
+osname = platform.system()
+
 
 def shutdown(time: int = 20) -> None:
-    """Gives a shutdown request to the system with the specified time"""
+    """Schedules a Shutdown after the specified time"""
 
-    # For Windows, time should be given in seconds
-    # For MacOS and Linux based distributions, time should be given in minutes
-
-    osname = platform.system()
     if "window" in osname.lower():
         cont = f"shutdown -s -t {time}"
         error_code = os.system(cont)
         # Here 1115 is the error code of scheduled shutdown.
         if error_code in [winerror.ERROR_SHUTDOWN_IN_PROGRESS, 1115]:
             print(
-                "A shutdown process has already been scheduled...\nIgnoring this process")
+                "A shutdown process has already been scheduled...\nIgnoring this process"
+            )
         else:
             print(f"Your System will shutdown in {time} seconds")
 
@@ -33,18 +33,21 @@ def shutdown(time: int = 20) -> None:
 
     else:
         raise Warning(
-            "This function is for Windows, Mac and Linux users only, can't execute on {}".format(osname))
+            "This function is for Windows, Mac and Linux users only, can't execute on {}".format(
+                osname
+            )
+        )
 
 
 def cancel_shutdown() -> None:
-    """Will cancel the scheduled shutdown"""
+    """Cancels the scheduled Shutdown"""
 
-    osname = platform.system()
     if "window" in osname.lower():
         error_code = os.system("shutdown /a")
         if error_code == winerror.ERROR_NO_SHUTDOWN_IN_PROGRESS:
             print(
-                "ShutDown cancellation process has been aborted! [NO shutdown scheduled]")
+                "ShutDown cancellation process has been aborted! [NO shutdown scheduled]"
+            )
         else:
             print("ShutDown has been cancelled")
 
@@ -58,4 +61,5 @@ def cancel_shutdown() -> None:
 
     else:
         raise Warning(
-            f"This function is for Windows and Linux only, can't execute on: {osname}")
+            f"This function is for Windows and Linux only, can't execute on: {osname}"
+        )
