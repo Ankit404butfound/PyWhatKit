9cd8a4c168e365834ada0be96a66b1bf5fceb04a
aaryanrr <raiaryan2422@gmail.com>
2021-09-05 20:50:59 +0530

    ⚡️ Removed Duplicate Code

---
+++ pywhatkit/core/core.py
@@ -0,0 +1,72 @@
+import time
+from platform import system
+from webbrowser import open
+from urllib.parse import quote
+
+import requests
+from pyautogui import hotkey, alert, typewrite, press, size, click
+
+from pywhatkit.core.exceptions import InternetException
+
+
+def check_number(number: str) -> bool:
+    """Checks the Number to see if contains the Country Code"""
+
+    return "+" in number or "_" in number
+
+
+def close_tab(wait_time: int = 2) -> None:
+    """Closes the Currently Opened Browser Tab"""
+
+    time.sleep(wait_time)
+    if system().lower() in ("windows", "linux"):
+        hotkey("ctrl", "w")
+    elif system().lower() in "darwin":
+        hotkey("command", "w")
+    else:
+        raise Warning(f"{system().lower()} not supported!")
+    press("enter")
+
+
+def check_connection() -> None:
+    """Check the Internet connection of the Host Machine"""
+
+    try:
+        requests.get("https://google.com")
+    except requests.RequestException:
+        raise InternetException(
+            f"Error while connecting to the Internet. Make sure you are connected to the Internet!"
+        )
+
+
+def check_window() -> None:
+    """Check if the browser Window is maximized or not"""
+
+    open("https://www.google.com")
+    alert(
+        "If the browser's window is not maximized,\nMaximise and then close it if you want,\nor send message "
+        "functions will not work",
+        "Pywhatkit",
+    )
+
+
+def send_message(message: str, receiver: str, wait_time: int) -> None:
+    """Parses and Sends the Message"""
+
+    if check_number(receiver):
+        open(
+            "https://web.whatsapp.com/send?phone="
+            + receiver
+            + "&text="
+            + quote(message)
+        )
+    else:
+        open("https://web.whatsapp.com/accept?code=" + receiver)
+
+    time.sleep(2)
+    width, height = size()
+    click(width / 2, height / 2)
+    time.sleep(wait_time - 2)
+    if not check_number(receiver):
+        typewrite(message)
+    press("enter")
