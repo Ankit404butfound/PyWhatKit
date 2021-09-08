b874cab2d23345da2487bdf767e41dc2e999e7cb
aaryanrr <raiaryan2422@gmail.com>
2021-09-08 22:14:31 +0530

    ⬆️ Update Dependencies

--- setup.py
+++ setup.py
@@ -3,38 +3,33 @@ from distutils.core import setup
 
 
 def readme() -> str:
-    with open(r'README.md') as f:
+    with open(r"README.md") as f:
         README = f.read()
     return README
 
 
 setup(
-    name='pywhatkit',
+    name="pywhatkit",
     packages=setuptools.find_packages(),
-    version='5.1',
-    license='MIT',
-    description='PyWhatKit is a Python library for Sending whatsapp message at certain time, it has several other features too.',
-    author='Ankit Raj Mahapatra',
-    author_email='ankitrajjitendra816@gmail.com',
-    url='https://github.com/Ankit404butfound/PyWhatKit',
-    download_url='https://github.com/Ankit404butfound/PyWhatKit/archive/refs/tags/5.0.zip',
-    keywords=['sendwhatmsg', 'info', 'playonyt', 'search', 'watch_tutorial'],
-    install_requires=[
-        'pyautogui',
-        'wikipedia',
-        'requests',
-        'Pillow'
-    ],
+    version="5.2",
+    license="MIT",
+    description="PyWhatKit is a Python library for Sending whatsapp message at certain time, it has several other features too.",
+    author="Ankit Raj Mahapatra",
+    author_email="ankitrajjitendra816@gmail.com",
+    url="https://github.com/Ankit404butfound/PyWhatKit",
+    download_url="https://github.com/Ankit404butfound/PyWhatKit/archive/refs/tags/5.0.zip",
+    keywords=["sendwhatmsg", "info", "playonyt", "search", "watch_tutorial"],
+    install_requires=["pyautogui", "wikipedia", "requests", "Pillow", "pyperclip"],
     include_package_data=True,
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
