059603191b620bd26938b1ee4f8528d8585629b9
aaryanrr <raiaryan2422@gmail.com>
2021-09-10 14:21:03 +0530

    ✨ Reusable Functions for Sending Image

--- pywhatkit/core/core.py
+++ pywhatkit/core/core.py
@@ -1,13 +1,18 @@
+import os
 import time
+import pathlib
 import pyperclip
 from platform import system
 from webbrowser import open
 from urllib.parse import quote
 
 import requests
-from pyautogui import hotkey, alert, press, click, size
+from pyautogui import hotkey, press, click, size
 
 from pywhatkit.core.exceptions import InternetException
+
+
+WIDTH, HEIGHT = size()
 
 
 def check_number(number: str) -> bool:
@@ -40,21 +45,9 @@ def check_connection() -> None:
         )
 
 
-def check_window() -> None:
-    """Check if the browser Window is maximized or not"""
-
-    open("https://www.google.com")
-    alert(
-        "If the browser's window is not maximized,\nMaximise and then close it if you want,\nor send message "
-        "functions will not work",
-        "Pywhatkit",
-    )
-
-
-def send_message(message: str, receiver: str, wait_time: int) -> None:
-    """Parses and Sends the Message"""
-
-    if check_number(receiver):
+def _web(receiver: str, message: str) -> None:
+    """Opens WhatsApp Web based on the Receiver"""
+    if check_number(number=receiver):
         open(
             "https://web.whatsapp.com/send?phone="
             + receiver
@@ -64,13 +57,76 @@ def send_message(message: str, receiver: str, wait_time: int) -> None:
     else:
         open("https://web.whatsapp.com/accept?code=" + receiver)
 
-    if not check_number(receiver):
+
+def send_message(message: str, receiver: str, wait_time: int) -> None:
+    """Parses and Sends the Message"""
+
+    _web(receiver=receiver, message=message)
+    if not check_number(number=receiver):
         pyperclip.copy(message)
-    width, height = size()
     time.sleep(wait_time)
-    click(width / 2, height / 2)
+    click(WIDTH / 2, HEIGHT / 2)
     if system().lower() == "darwin":
         hotkey("command", "v")
     else:
+        hotkey("ctrl", "v")
+    press("enter")
+
+
+def copy_image(path: str) -> None:
+    """Copy the Image to Clipboard based on the Platform"""
+
+    if system().lower() == "linux":
+        if pathlib.Path(path).suffix in (".PNG", ".png"):
+            os.system(f"xclip -selection clipboard -target image/png -i {path}")
+        elif pathlib.Path(path).suffix in (".jpg", ".JPG", ".jpeg", ".JPEG"):
+            os.system(f"xclip -selection clipboard -target image/jpg -i {path}")
+        else:
+            raise Exception(
+                f"File Format {pathlib.Path(path).suffix} is not Supported!"
+            )
+    elif system().lower() == "windows":
+        import win32clipboard
+        from io import BytesIO
+
+        from PIL import Image
+
+        image = Image.open(path)
+        output = BytesIO()
+        image.convert("RGB").save(output, "BMP")
+        data = output.getvalue()[14:]
+        output.close()
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+        win32clipboard.CloseClipboard()
+    elif system().lower() == "darwin":
+        if pathlib.Path(path).suffix in (".jpg", ".jpeg", ".JPG", ".JPEG"):
+            os.system(
+                f"osascript -e 'set the clipboard to (read (POSIX file \"{path}\") as JPEG picture)'"
+            )
+        else:
+            raise Exception(
+                f"File Format {pathlib.Path(path).suffix} is not Supported!"
+            )
+    else:
+        raise Exception(f"Unsupported System: {system().lower()}")
+
+
+def send_image(path: str, caption: str, receiver: str, wait_time: int) -> None:
+    """Sends the Image to a Contact or a Group based on the Receiver"""
+
+    copy_image(path=path)
+    _web(message=caption, receiver=receiver)
+
+    time.sleep(wait_time)
+    click(WIDTH / 2, HEIGHT / 2)
+    if system().lower() == "darwin":
+        hotkey("command", "v")
+    else:
+        hotkey("ctrl", "v")
+
+    if not check_number(number=receiver):
+        pyperclip.copy(caption)
         hotkey("ctrl", "v")
     press("enter")
