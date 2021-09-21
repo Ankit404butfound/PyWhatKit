2475d34d691e9dc407ca101d2386f76afe07b721
aaryanrr <raiaryan2422@gmail.com>
2021-09-22 00:04:47 +0530

    📝 Better Function Description

--- pywhatkit/handwriting.py
+++ pywhatkit/handwriting.py
@@ -4,7 +4,7 @@ import requests
 def text_to_handwriting(
     string: str, save_to: str = "pywhatkit.png", rgb: tuple = (0, 0, 138)
 ) -> None:
-    """Convert the given str to handwriting"""
+    """Convert the given String to Handwritten Characters"""
 
     data = requests.get(
         "https://pywhatkit.herokuapp.com/handwriting?text=%s&rgb=%s,%s,%s"
