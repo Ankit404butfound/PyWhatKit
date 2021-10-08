45d1d23c2c428a29dccd195829dda64f214edb43
Sachit <72592470+Sachit71@users.noreply.github.com>
2021-10-09 01:49:36 +0530

    Update webss.py

--- pywhatkit/webss.py
+++ pywhatkit/webss.py
@@ -2,10 +2,10 @@ import requests
 
 def web_ss(weblink):
     BASE = 'https://render-tron.appspot.com/screenshot/'
-    url = weblink
-    path = 'me.jpg'
-    response = requests.get(BASE + url, stream=True)
-    # save file, see https://stackoverflow.com/a/13137873/7665691
+    url = weblink #enters website to be screenshotted
+    path = 'me.jpg' #image name is saved as
+    response = requests.get(BASE + url, stream=True)# uses requests to complete action
+
     if response.status_code == 200:
         with open(path, 'wb') as file:
             for chunk in response:
