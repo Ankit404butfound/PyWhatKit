83eb902b48f2e64086f5470ec03d7c83560b06a8
Sachit <72592470+Sachit71@users.noreply.github.com>
2021-10-10 20:42:32 +0530

    Update misc.py

--- pywhatkit/misc.py
+++ pywhatkit/misc.py
@@ -80,13 +80,3 @@ def search(topic: str) -> None:
     link = "https://www.google.com/search?q={}".format(topic)
     web.open(link)
  
-def web_ss(weblink):
-    BASE = 'https://render-tron.appspot.com/screenshot/'
-    url = weblink #enters website to be screenshotted
-    path = 'me.jpg' #image name is saved as
-    response = requests.get(BASE + url, stream=True)# uses requests to complete action
-
-    if response.status_code == 200:
-        with open(path, 'wb') as file:
-            for chunk in response:
-                file.write(chunk)
