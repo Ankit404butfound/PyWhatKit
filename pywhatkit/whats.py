37fd52d23a79e37202624dfc5cb3b1fb05a8e9f5
kineolyan <kineolyan@protonmail.com>
2021-10-16 14:18:05 +0200

    Changes all whatsapp functions to accept an item or a list of items to send.

--- pywhatkit/whats.py
+++ pywhatkit/whats.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime
 from urllib.parse import quote
-from typing import Optional
+from typing import Optional, Union, List, Tuple, TypeVar
 
 import webbrowser as web
 import pyautogui as pg
@@ -12,45 +12,54 @@ pg.FAILSAFE = False
 
 core.check_connection()
 
+T = TypeVar("T")
+def input_to_list(value_input: Union[T, List[T]]) -> List[T]:
+    if type(value_input) is list:
+        return value_input
+    return list(value_input) 
+
+def _log_message_sent(*, message: str, receiver: str) -> None:
+    log.log_message(_time=time.localtime(), receiver=receiver, message=message)
+
+def _do_send_message(
+    *,
+    phone_no: str,
+    message: str,
+    wait_time: int) -> None:
+    core.send_message(message, phone_no, wait_time)
+    pg.press("enter")
+    _log_message_sent(receiver=phone_no, message=message)
+
+# Custom message type in case we want to create a new format later, like images that need a path and a caption
+Message = str
+MessageInput = Union[Message, List[Message]]
 
 def sendwhatmsg_instantly(
     phone_no: str,
-    message: str,
+    messages: MessageInput,
     wait_time: int = 20,
     tab_close: bool = False,
     close_time: int = 3,
 ) -> None:
     """Send WhatsApp Message Instantly"""
+    messages = input_to_list(messages)
 
+    # Check once the phone number
     if not core.check_number(number=phone_no):
         raise exceptions.CountryCodeException("Country code missing from phone_no")
 
-    web.open(
-        "https://web.whatsapp.com/send?phone=" + phone_no + "&text=" + quote(message)
-    )
-    time.sleep(4)
-    pg.click(core.WIDTH / 2, core.HEIGHT / 2)
-    time.sleep(wait_time-4)
-    pg.press("enter")
-    log.log_message(_time=time.localtime(), receiver=phone_no, message=message)
-    if tab_close:
-        core.close_tab(wait_time=close_time)
-
-
-def sendwhatmsg(
-    phone_no: str,
-    message: str,
-    time_hour: int,
-    time_min: int,
-    wait_time: int = 20,
-    tab_close: bool = False,
-    close_time: int = 3,
-) -> None:
-    """Sends a WhatsApp Message"""
-
-    if not core.check_number(number=phone_no):
-        raise exceptions.CountryCodeException("Country code missing from Phone Number!")
-
+    for message in messages:
+        _do_send_message(message=message, phone_no=phone_no, wait_time=wait_time)
+        if tab_close:
+            core.close_tab(wait_time=close_time)
+            
+def _wait_until_time(
+    *,
+    target_time: Tuple[int, int],
+    wait_time: int
+):
+    "Waits until the given hour"
+    time_hour, time_min = target_time
     if time_hour not in range(25) or time_min not in range(60):
         raise Warning("Invalid Time Format")
 
@@ -73,48 +82,56 @@ def sendwhatmsg(
         f"be delivered!"
     )
     time.sleep(sleep_time)
-    core.send_message(message=message, receiver=phone_no, wait_time=wait_time)
-    log.log_message(_time=current_time, receiver=phone_no, message=message)
-    if tab_close:
-        core.close_tab(wait_time=close_time)
+
+def sendwhatmsg(
+    phone_no: str,
+    messages: MessageInput,
+    time_hour: int,
+    time_min: int,
+    wait_time: int = 20,
+    tab_close: bool = False,
+    close_time: int = 3,
+) -> None:
+    """Sends one or many WhatsApp Messages"""
+    messages = input_to_list(messages)
+
+    if not core.check_number(number=phone_no):
+        raise exceptions.CountryCodeException("Country code missing from Phone Number!")
+
+    _wait_until_time(time=(time_hour, time_min), wait_time=wait_time)
+
+    for message in messages:
+        core.send_message(message=message, receiver=phone_no, wait_time=wait_time)
+        _log_message_sent(receiver=phone_no, message=message)
+        if tab_close:
+            core.close_tab(wait_time=close_time)
 
 
 def sendwhatmsg_to_group(
     group_id: str,
-    message: str,
+    messages: MessageInput,
     time_hour: Optional[int] = None,
     time_min: Optional[int] = None,
     wait_time: int = 20,
     tab_close: bool = False,
     close_time: int = 3,
 ) -> None:
-    """Send WhatsApp Message to a Group"""
+    """Send one or more WhatsApp Messages to a Group.
+    
+    If an time is defined, this will wait for the correct time before sending the messages.
+    """
+    messages = input_to_list(messages)
 
-    if time_hour not in range(25) or time_min not in range(60):
-        raise Warning("Invalid time format")
+    if time_hour is not None and time_min is not None:
+        _wait_until_time(target_time=(time_hour, time_min), wait_time=wait_time)
+    elif (time_hour is None) != (time_min is None):
+        raise Exception(f"Invalid time: some elements are not defined {time_hour}h{time_min}")
 
-    current_time = time.localtime()
-    left_time = datetime.strptime(
-        f"{time_hour}:{time_min}:0", "%H:%M:%S"
-    ) - datetime.strptime(
-        f"{current_time.tm_hour}:{current_time.tm_min}:{current_time.tm_sec}",
-        "%H:%M:%S",
-    )
-
-    if left_time.seconds < wait_time:
-        raise exceptions.CallTimeException(
-            "Call time must be greater than wait_time as web.whatsapp.com takes some time to load"
-        )
-
-    sleep_time = left_time.seconds - wait_time
-    print(
-        f"In {sleep_time} seconds web.whatsapp.com will open and after {wait_time} seconds message will be delivered"
-    )
-    time.sleep(sleep_time)
-    core.send_message(message=message, receiver=group_id, wait_time=wait_time)
-    log.log_message(_time=current_time, receiver=group_id, message=message)
-    if tab_close:
-        core.close_tab(wait_time=close_time)
+    for message in messages:
+        core.send_message(message=message, receiver=group_id, wait_time=wait_time)
+        _log_message_sent(receiver=group_id, message=message)
+        if tab_close:
+            core.close_tab(wait_time=close_time)
 
 
 def sendwhats_image(
@@ -125,7 +142,20 @@ def sendwhats_image(
     tab_close: bool = False,
     close_time: int = 3,
 ) -> None:
-    """Send Image to a WhatsApp Contact"""
+    sendwhats_image(receiver, [(img_path, caption)], wait_time, tab_close, close_time)
+
+Image = Tuple[str, str]
+ImageInput = Union[Image, List[Image]]
+
+def sendwhats_images(
+    receiver: str,
+    images: ImageInput,
+    wait_time: int = 15,
+    tab_close: bool = False,
+    close_time: int = 3,
+) -> None:
+    """Send one or more Images to a WhatsApp Contact"""
+    images = input_to_list(images)
 
     if (
         not receiver.isalpha()
@@ -134,13 +164,14 @@ def sendwhats_image(
     ):
         raise exceptions.CountryCodeException("Country Code missing from Phone Number!")
 
-    current_time = time.localtime()
-    core.send_image(
-        path=img_path, caption=caption, receiver=receiver, wait_time=wait_time
-    )
-    log.log_image(_time=current_time, path=img_path, receiver=receiver, caption=caption)
-    if tab_close:
-        core.close_tab(wait_time=close_time)
+    for (img_path, caption) in images:
+        current_time = time.localtime()
+        core.send_image(
+            path=img_path, caption=caption, receiver=receiver, wait_time=wait_time
+        )
+        log.log_image(_time=current_time, path=img_path, receiver=receiver, caption=caption)
+        if tab_close:
+            core.close_tab(wait_time=close_time)
 
 
 def open_web() -> bool:
