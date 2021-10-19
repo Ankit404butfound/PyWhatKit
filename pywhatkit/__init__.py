a58675ed3eaa568f135b328e0dff27a14adbbb2d
kineolyan <kineolyan@protonmail.com>
2021-10-19 21:00:14 +0200

    Correct files to pass black checks.

--- pywhatkit/__init__.py
+++ pywhatkit/__init__.py
@@ -13,8 +13,13 @@ from pywhatkit.handwriting import text_to_handwriting
 from pywhatkit.mail import send_hmail, send_mail
 from pywhatkit.misc import info, playonyt, search, show_history
 from pywhatkit.sc import cancel_shutdown, shutdown
-from pywhatkit.whats import (open_web, sendwhatmsg, sendwhatmsg_instantly,
-                             sendwhatmsg_to_group, sendwhats_image)
+from pywhatkit.whats import (
+    open_web,
+    sendwhatmsg,
+    sendwhatmsg_instantly,
+    sendwhatmsg_to_group,
+    sendwhats_image,
+)
 
 if system().lower() in ("darwin", "windows"):
     from pywhatkit.misc import take_screenshot
