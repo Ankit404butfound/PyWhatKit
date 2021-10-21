8a1350ae9c8bd5c0168743ef3c3e85d8b41c0672
Sijey <cjpraveen@hotmail.com>
2021-10-21 04:01:49 -0700

    Added playonSpotify Function 🌟

--- pywhatkit/misc.py
+++ pywhatkit/misc.py
@@ -1,7 +1,7 @@
 import time
 import webbrowser as web
 from platform import system
-from typing import Optional, Union
+from typing import Optional, Unin
 from PIL import ImageGrab
 import wikipedia
 from requests import get
