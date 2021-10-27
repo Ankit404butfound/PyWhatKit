6d50793366c172f34bc6c9cb28326dce95495bff
Naman sharma <namansharma232009@gmail.com>
2021-10-27 16:46:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author="Ankit Raj Mahapatra",
     author_email="ankitrajjitendra816@gmail.com",
     url="https://github.com/Ankit404butfound/PyWhatKit",
-    download_url="https://github.com/Ankit404butfound/PyWhatKit/archive/refs/tags/5.2.zip", # not yet released
+    download_url="https://github.com/Ankit404butfound/PyWhatKit/archive/refs/tags/5.2.zip",
     keywords=["sendwhatmsg", "info", "playonyt", "search", "watch_tutorial"],
     install_requires=["pyautogui", "wikipedia", "requests", "Pillow", "pyperclip"],
     include_package_data=True,
