232d1a7d6a2469209367e0afe7357d6f1295cd37
Naman <namansharma232009@gmail.com>
2022-01-03 11:49:13 +0530

    fixed unabletoaccessapi exception in handwriting.py

--- pywhatkit/handwriting.py
+++ pywhatkit/handwriting.py
@@ -4,16 +4,17 @@ from pywhatkit.core import exceptions
 
 
 def text_to_handwriting(
-    string: str, save_to: str = "pywhatkit.png", rgb: tuple = (0, 0, 0)
+        string: str, save_to: str = "pywhatkit.png", rgb: tuple = (0, 0, 0)
 ) -> None:
     """Convert the given String to Handwritten Characters"""
 
-    try:
-        data = requests.get(
-            f"https://pywhatkit.herokuapp.com/handwriting?text={string}&rgb={rgb[0]},{rgb[1]},{rgb[2]}"
-        ).content
+    data = requests.get(
+        f"https://pywhatkit.herokuapp.com/handwriting?text={string}&rgb={rgb[0]},{rgb[1]},{rgb[2]}"
+    )
+    status_code = data.status_code  # source = https://www.geeksforgeeks.org/http-status-codes-successful-responses/
+    if status_code == 200:
         with open(save_to, "wb") as file:
-            file.write(data)
-        file.close()
-    finally:
-        raise exceptions.UnableToAccessApi("Unable to access pywhatkit api right now")
+            file.write(data.content)
+            file.close()
+    elif 400 <= status_code <= 599:
+        raise exceptions.UnableToAccessApi("Unable to access Pywhatkit api right now")
