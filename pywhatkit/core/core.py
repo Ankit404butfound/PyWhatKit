42c3e667b5ab4e17bb88806de86608edd3d7ce61
Pramod Kumar <pramodkumar.damam73@gmail.com>
2022-05-05 12:58:15 +0530

    Don't click center of the screen. Click right most center

--- pywhatkit/core/core.py
+++ pywhatkit/core/core.py
@@ -82,7 +82,7 @@ def send_message(message: str, receiver: str, wait_time: int) -> None:
 
     _web(receiver=receiver, message=message)
     time.sleep(7)
-    click(WIDTH / 2, HEIGHT / 2)
+    click(WIDTH - 10, HEIGHT / 2)
     time.sleep(wait_time - 7)
     if not check_number(number=receiver):
         for char in message:
